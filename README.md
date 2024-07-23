37f4a10c1741bffc59b18bcb0fff8467fb594975
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2024-07-23 19:48:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sushi-Japneese Restaurant 
+ This is Japneese Restaurant Website build using HTML , CSS and JavaScript 
---
+++ js/script.js
@@ -0,0 +1,33 @@
+// import images as relative image path won't work vite/vercel.
+// import check from '../assets/check.svg'
+// import star from '../assets/star.svg'
+// import sushi12 from '../assets/sushi-12.svg'
+// import sushi11 from '../assets/sushi-11.svg'
+// import sushi10 from '../assets/sushi-10.svg'
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// int AOS animation
+AOS.init({
+    duration: 100,
+    offset: 100,
+});
+
+// const trendingSushis = [
+//     'Make Sushi',
+//     'Nigiri Sushi',
+//     'Oshizushi',
+//     'Temaki Sushi',
+//     'Uramaki Sushi',
+//     'Inari Sushi',
+// ];
+
+// const trendingDrinks = [
+//     "Oruncha",
+//     "Ofukucha",
+//     "Sakura Tea",
+//     "Kombu-cha",
+//     "Aojiru",
+//     "Mugicha",
+// ]
